7c6c0685d8692f44c10ec2e3b1c21e4a4c6d78f3
Mohamed-ZOUMA <zoumamohamed442@gmail.com>
2024-12-20 12:10:52 -0800

    Add files via upload
    cette partie est le partie de Mohamed Houssein Ahmed qui conserne les 4 premier interface apres avoir clique le pouton client

---
+++ Login_client.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription Form</title>
+    <link rel="stylesheet" href="acceuill22.css">
+</head>
+<body>
+    <div class="main"> <!--main --> 
+        <div class="couleur">
+        <div class="navbar"><!--main/navbar -->
+             <div class="menu"><!--main/navbar/menu -->
+               <div class="top">
+           <img class="img" src="dj.jpg" height="35px" width="50px" align="left">
+                <center>  <ul>
+                   <li><a href="acceuil2.html">ACCEUIL</a></li>
+                   <li><a href="propos.html">A PROPOS</a></li>
+                   <li><a href="service1.html">SERVICE</a></li>
+                   <li><a href="contact.html">CONTACT</a></li>
+                </ul></center> 
+                <img class="img" src="dj.jpg" height="35px" width="50px" align="right">
+            </div>
+               
+             </div><!--main/navbar -->
+
+                 <!--main/navbar -->
+
+       </div> <!--main -->
+
+       <style>
+
+        .form{
+         text-align: center;
+         margin-left: 415px; 
+        
+         }
+
+         h1{
+
+            margin-left: 35px;
+         }
+         a{
+            color: chartreuse;
+         }
+ 
+     </style>
+
+<?php
+    $server = 'localhost';
+    $utilisateur = 'root';
+    $motpasse = '';
+    $base = 'gestion_train';
+
+    $connection = mysqli_connect($server, $utilisateur, $motpasse, $base);
+
+    
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $ID = $_POST['ID'];
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $telephone = $_POST['num'];
+        $email = $_POST['email'];
+        $mdp = $_POST['mdp'];
+        $cmdp = $_POST['cmdp'];
+
+        
+            
+        if($mdp != $cmdp  && $mdp != NULL ){ $message = "Confirmation incorrect !";}
+        else{
+
+           
+        // Requête pour vérifier l'administrateur et le mot de passe
+        $sql = "INSERT INTO passagers VALUES('$ID','$nom','$prenom','$telephone','$email','$mdp')";
+        $result = $connection->query($sql);
+
+        if ($result) {
+                $message = header("Location: R1.php");
+            } else {
+                // Ajouter un message d'erreur si le nom d'administrateur ou le mot de passe est incorrect
+                $message = "Nom d'administrateur ou mot de passe incorrect.";
+            }
+        } 
+        
+    
+    
+        // Fermer la connexion à la base de données
+        $connection->close();
+        }
+    
+    ?>
+
+            <div class="content"><!--main/content -->
+                <h1><span>Inscription</span></h1>
+                
+                <form action="Login_client.php" method="post" >
+                    <div class="form" >
+
+                    <?php if (!empty($message)): ?>
+                    <?php echo $message; ?>
+                    <?php endif; ?>
+
+                    <br><br>
+
+                       <center> <h2> INSCRIVEZ VOUS !</h2> </center> 
+                       
+                       <input type="text" name="ID" placeholder="Entrer votre ID" required>
+                        <input type="text" name="nom" placeholder="Entrer votre nom"required>
+                        <input type="text" name="prenom" placeholder="Entrer votre prenom"required>
+                        <input type="text" name="num" placeholder="Entrer votre contact"required>
+                        <input type="email" name="email" placeholder="Entrer votre Email"required>
+                        <input type="password" name="mdp" placeholder="Enter votre mot de passe" required>
+                        <input type="password" name="cmdp" placeholder="Confirmer votre mot de passe"required>
+
+                        <button type="submit" class="btnn"> S'inscrire </button> 
+                         <button type="submit" class="btnn"><a href="clie.html">  Revenir </button></a>
+                        <p class="liw"> Déja Inscrit ? <a href="login_P.php">Se connecter ici !</a></p>
+                    </div>
+                </form>
+   
+                
+            </div>
+
+            <div class="icons">
+                <!-- Your existing social media icons code -->
+            </div>
+        </div><!--main -->
+    </div>
+
+</body>
+</html>
---
+++ login_P.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="acceuill22.css">
+</head>
+<body>
+    <div class="main2"> <!--main -->
+        <div class="couleur">
+        <div class="navbar"><!--main/navbar -->
+             <div class="menu"><!--main/navbar/menu -->
+               <div class="top">
+           <img class="img" src="dj.jpg" height="35px" width="50px" align="left">
+                <center>  <ul>
+                   <li><a href="acceuil2.html">Acceuil</a></li>
+                   <li><a href="about.html">A Propros</a></li>
+                   <li><a href="service.html">SERVICE</a></li>
+                   
+                   <li><a href="contacte.html">CONTACT</a></li>
+                </ul></center> 
+                <img class="img" src="dj.jpg" height="35px" width="50px" align="right">
+            </div>
+               
+             </div><!--main/navbar -->
+
+                 <!--main/navbar -->
+
+       </div> <!--main -->
+       <div class="content"><!--main/content -->
+           <h1><span>Connexion</span></h1>
+         
+         <style>
+
+           .form {
+            text-align: center;
+            margin-left: 415px; 
+           
+            }
+
+            .message{
+                  margin-left:50%;
+                  margin-bottom:-50%;
+                padding-top: -100px;
+            color: white;
+            text-align: center;
+            }
+
+           
+        </style>
+
+<?php
+    $server = 'localhost';
+    $utilisateur = 'root';
+    $motpasse = '';
+    $base = 'gestion_train';
+
+    $connection = mysqli_connect($server, $utilisateur, $motpasse, $base);
+
+    $message = '';  // Variable pour stocker le message d'erreur
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $number = $_POST["username"];
+        $mdp = $_POST["password"];
+
+        session_start();
+
+// Stocker une variable dans la session
+
+$_SESSION['username'] = $number;
+
+        // Requête pour vérifier l'administrateur et le mot de passe
+        $sql = "SELECT * FROM passagers WHERE passager_id  = '$number' AND mdp = '$mdp'";
+        $result = $connection->query($sql);
+
+        if ($result) {
+            // Vérifier le nombre de lignes retournées
+            if ($result->num_rows > 0) {
+             
+                $message = header("Location: R1.php");
+              
+            } else {
+                // Ajouter un message d'erreur si le nom d'administrateur ou le mot de passe est incorrect
+                $message = "ID ou mot de passe incorrect.";
+            }
+        } else {
+            $message = "Erreur lors de l'exécution de la requête : " . $connection->error;
+        }
+
+        // Fermer la connexion à la base de données
+        $connection->close();
+        
+    }
+    ?>
+
+
+           
+<form action="R1.php" method="POST">
+               <div class="form">
+               
+<?php if (!empty($message)): ?>
+<?php echo $message; ?>
+<?php endif; ?>
+<br>
+                <h2>Login Here</h2>
+                
+                <input type="text" name="username" placeholder="Entrer Votre Identifiant Ici">
+                <input type="password" name="password" placeholder="Entrer Votre Mot De Passe Ici">
+                <button type="submit" class="btnn"><a href="paiement.php">Se Connecter</button></a>
+                <button type="reset" class="btnn"><a href="#">Annuler</a></button>
+                <button type="" class="btnn"><a href="paiement.php">Revenir</button></a></div>
+</form>
+
+
+
+             
+
+                    <div class="icons">
+                    <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
+                </div>
+
+                
+
+         
+           
+            
+
+       
+
+    </div>
+    
+
+</div>
+
+    
+    </div>
+    
+
+</div>
+
+             
+                   </div><!--main -->
+    
+   
+</body>
+</html>
